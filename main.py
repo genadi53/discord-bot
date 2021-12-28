ab5049cdbb74dfb69a219d02a2fcc029b233a466
genadi53 <kotka5353@gmail.com>
2021-12-28 17:55:03 +0200

    react with emoji on messages

--- main.py
+++ main.py
@@ -14,9 +14,30 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
     if(message.author == client.user): return
-    print(message)
-    await message.channel.send('Yo')
+
+    if(message.content == 'hello'): 
+        await message.channel.send('Welcome!')
+
+    if(message.content == 'cool'): 
+        await message.add_reaction('\U0001F60E')
+    # await message.channel.send('Yo')
+  
+@client.event
+async def on_message_edit(before, after):
+    if(before.author == client.user): return
+
+    await before.channel.send(
+    f'{before.author} edit a message\n'
+    f'Before: {before.content}\n'
+    f'After: {after.content}'
+    ) 
+
+@client.event
+async def on_reaction_add(reaction, user):
+   
+    await reaction.message.channel.send(
+    f'{user} reacted with {reaction.emoji}'
+    ) 
 
 client.run('OTI1MzE5NTUyMDc2NzQ2Nzgy.YcrY_A.dwNCXWaRTZ0OmK4aFw6CTTz9b0M')
